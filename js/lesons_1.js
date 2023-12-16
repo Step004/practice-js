e0c76b4113a5e2c5e07601a432ac646ff1c8e772
Step004 <ctepan.filep9@gmail.com>
2023-12-16 15:04:58 +0200

    1 leson

---
+++ js/lesons_1.js
@@ -0,0 +1,7 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+let name = "Stepan";
+let age = 19;
+let hobby = "JAVASCRIPT";
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+console.log(message);
