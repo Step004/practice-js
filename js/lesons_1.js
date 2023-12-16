483b75525de8f469dcb3b9c251df8113b0d13d44
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2023-12-16 15:16:09 +0200

    add task2

--- js/lesons_1.js
+++ js/lesons_1.js
@@ -1,7 +1,17 @@
-// ЗАДАЧА 1
-// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-let name = "Stepan";
-let age = 19;
-let hobby = "JAVASCRIPT";
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
-console.log(message);
+// // ЗАДАЧА 1
+// // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+// let name = "Stepan";
+// let age = 19;
+// let hobby = "JAVASCRIPT";
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
+// console.log(message);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+let name = prompt("Введіть своє ім'я");
+let emailAdress = prompt ("Введіть свою електронну пошту");
+let numberPhone = prompt ("Введіть свій номер телефону");
+
+const message2 = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ ${emailAdress} ПОЧТУ І ${numberPhone} ТЕЛЕФОН`;
+console.log(message2);
