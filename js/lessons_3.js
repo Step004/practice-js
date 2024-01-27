11af2983e74f8028712fa443e11a5ba6d6e8c5e5
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-01-27 15:50:12 +0200

    1 task

---
+++ js/lessons_3.js
@@ -0,0 +1,28 @@
+// Створіть карточки товару з масиву використовуючи createElement
+import car from "./data/cars.js";
+
+// const list = document.querySelector(".js-goods");
+
+// function createCard ({name, img, price, description}) {
+// const listItem = document.createElement("li");
+// const text = document.createElement("h2");
+// const picture = document.createElement("img");
+// const priceCar = document.createElement("p");
+// const textDes = document.createElement("p");
+
+// text.textContent = name;
+// picture.src = img;
+// picture.alt = name;
+// picture.width = 320;
+// priceCar.textContent = price;
+// textDes.textContent = description;
+
+// listItem.append(text, picture, priceCar, textDes);
+
+// return listItem;
+// }
+
+// const card = car.map(createCard);
+// console.log(card);
+
+// list.append(...card);
